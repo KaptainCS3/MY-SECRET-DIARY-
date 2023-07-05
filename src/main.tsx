b62f3ba960c3f689ffd9bb736fbb1265296eabe9
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-05 03:42:36 +0100

    wrap App to routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
-import { store } from './app/store'
+import {BrowserRouter} from 'react-router-dom'
+import store from './app/store'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
 <Provider store={store}>
-<App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 </Provider>
   </React.StrictMode>,
 )
