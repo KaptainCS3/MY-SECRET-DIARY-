03a5b5cd3c16e33b6059817c01adfc443901e386
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2023-07-05 21:35:47 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          DEFAULT: "#63004F",
+        },
+        partial: {
+          DEFAULT: "#CCCCCC",
+        },
+      },
+    },
+    screens: {
+      sm: { min: "200px", max: "768px" },
+      // => @media (min-width: 200px and max-width: 768px) { ... }
+      md: { min: "769px", max: "990px" },
+      // => @media (min-width: 769px and max-width: 990px) { ... }
+      lg: { min: "991px" },
+      // => @media (min-width: 991px ) { ... }
+    },
   },
   plugins: [],
 };
