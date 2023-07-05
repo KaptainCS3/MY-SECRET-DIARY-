990212715e071e7622c7e78dfb9834761ad7e150
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-05 03:24:52 +0100

    tweak firebase configs

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth , GoogleAuthProvider, FacebookAuthProvider} from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FBASE_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOM,
@@ -18,4 +13,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+export {auth, googleProvider, facebookProvider}
