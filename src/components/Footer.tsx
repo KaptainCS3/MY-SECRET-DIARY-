6e4192bb2c5554f51bad0ae573d84a687d43bae9
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-06 14:02:11 +0100

    initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="absolute bottom-0 flex justify-between bg-black w-full text-white px-6 py-3">
+        <span>DRTech &copy; 2023</span>
+        <span>Privacy policy<span className="border-l mx-2 border-white"></span>T &amp; C</span>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <div className="bg-black w-full py-4 mb-10">
+      <nav className="flex w-full justify-between items-center px-6">
+        <img src="/assets/logo.png" alt="logo" />
+        <h1 className="text-[1.7rem] text-white">My Private Diary</h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
