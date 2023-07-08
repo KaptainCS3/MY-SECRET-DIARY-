6e7289aed0d2b4c7d7cc7354562c6b5e578f07c8
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-08 22:03:37 +0100

    header props added

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,16 @@
-const Nav = () => {
+import React from "react";
+
+interface Props{
+  headerText: string;
+}
+const Nav: React.FC<Props> = ({headerText}) => {
   return (
     <div className="bg-black w-full py-4 mb-10">
-      <nav className="flex w-full justify-between items-center px-6">
+      <nav className="flex w-full items-center px-6">
         <img src="/assets/logo.png" alt="logo" />
-        <h1 className="text-[1.7rem] text-white">My Private Diary</h1>
+        <h1 className="text-[1.7rem] text-white pl-16 sm:text-xl">
+          {headerText}
+        </h1>
       </nav>
     </div>
   );
