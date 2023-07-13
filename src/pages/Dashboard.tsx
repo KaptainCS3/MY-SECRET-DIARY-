1edd4c2663f4c8987fa1520665d9e06b580aa868
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-13 23:35:13 +0100

    use custom button and redirect to diary entry

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,17 +2,24 @@ import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import SearchDiary from "../components/SearchDiary";
 import Diary from "../components/Diary";
+import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const newEntry = () => {
+    navigate("/new-entry");
+  };
   return (
-    <div className="w-full relative">
+    <div className="w-full">
       <Nav headerText="Home" />
-      <div className="mx-6">
+      <div className="mx-6 mb-14">
         <div className="-mt-5 flex justify-between items-center">
           <h2 className="text-xl font-bold">Welcome back</h2>
           <Button
+            actionBtn={newEntry}
+            styleProps="border px-4 py-2 rounded-md text-sm font-bold"
             textContent="New entry"
-            styleProps="bg-black text-white border px-4 py-2 rounded-md text-sm font-bold"
+            type="button"
           />
         </div>
         <SearchDiary />
