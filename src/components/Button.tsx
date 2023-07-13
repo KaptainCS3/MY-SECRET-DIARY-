a123ae672da5e16438fa30cf2e878f725d212f24
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-13 23:28:12 +0100

    customize button with props inputs

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,19 @@
 interface Props {
   textContent: string;
   styleProps: string;
+  type: "button" | "submit" | "reset";
+  actionBtn: () => void
 }
-const Button = ({ textContent, styleProps }: Props) => {
-  return <button className={styleProps}>{textContent}</button>;
+const Button = ({ textContent, styleProps, type, actionBtn }: Props) => {
+  return (
+    <button
+      onClick={actionBtn}
+      type={type}
+      className={`bg-black text-white  ${styleProps}`}
+    >
+      {textContent}
+    </button>
+  );
 };
 
 export default Button;
