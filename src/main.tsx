4017cd3f3f2c8a54a707276575071f6415a30ea8
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-14 00:51:54 +0100

    minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx'
 import { Provider } from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
 import store from './app/store'
+// import {ToastContainer} from "react-toastify"
+import "react-toastify/ReactToastify.css"
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
 <Provider store={store}>
