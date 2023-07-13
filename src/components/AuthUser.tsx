c634cd40de79f1000bbb0a716eca3c69f6b53095
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-14 00:47:22 +0100

    change file directory

---
+++ src/components/AuthUser.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/hook";
+import { RootState } from "../app/store";
+import { signInWithFacebook, signInWithGoogle } from "../features/UserSlice";
+import LoginBTN from "./LoginBTN";
+import { useNavigate } from "react-router-dom";
+const AuthUser = () => {
+  const user = useAppSelector((state: RootState) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleSignInWithFacebook = () => {
+    dispatch(signInWithFacebook());
+  };
+
+  const handleSignInWithGoogle = () => {
+    dispatch(signInWithGoogle());
+  };
+
+  useEffect(() => {
+    if (user.user !== null) {
+      navigate("/dashboard");
+    }
+  }, [user.user, navigate]);
+  // imgSrc = "/assets/google.png";
+  // imgSrc = "/assets/facebook.png";
+  return (
+    <div className="w-full">
+      <LoginBTN
+        handleLogin={handleSignInWithFacebook}
+        btnText="Sign in with Facebook"
+      />
+      <LoginBTN
+        handleLogin={handleSignInWithGoogle}
+        btnText="Sign in with Google"
+      />
+    </div>
+  );
+};
+
+export default AuthUser;
