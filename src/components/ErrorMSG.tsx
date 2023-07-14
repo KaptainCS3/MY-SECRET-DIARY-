4bf68e0bbf19593abdbf5c36518f3a75a6ae8dd2
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-14 10:47:11 +0100

    catch feature error from component

--- src/components/ErrorMSG.tsx
+++ src/components/ErrorMSG.tsx
@@ -1,9 +1,16 @@
 interface Props {
-  error_value: string;
+  error_value: string | string[];
 }
 const ErrorMSG = ({ error_value }: Props) => {
-
-  return <div className="text-isPublic pt-3">{error_value}</div>;
+  if (!error_value) return null;
+  const errors = Array.isArray(error_value) ? error_value : [error_value];
+  return (
+    <div className="text-isPublic pt-3">
+      {errors.map((errorMsg) => (
+        <p key={errorMsg}>{errorMsg}</p>
+      ))}
+    </div>
+  );
 };
 
 export default ErrorMSG;
