91b48bc659dd50befd28bc784f6a6a41613ce1c7
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-21 03:09:35 +0100

    initial commit

---
+++ src/features/DiaryList.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+interface DiaryList {
+  id: string;
+  image: string;
+  category: string;
+  description: string;
+  isPublic: boolean;
+  createdDate: Date;
+  // formattedDate: string;
+  // formattedTime: string;
+}
+interface DiaryState {
+  list: DiaryList[];
+}
+const initialState: DiaryState = {
+  list: [],
+};
+
+const storedList = createSlice({
+    name: 'diaryList',
+    initialState,
+    reducers:{
+        diaryListItems:(state, action: PayloadAction<DiaryList[]>) =>{
+            state.list = (action.payload);
+        }
+    }
+})
+
+export const {diaryListItems} = storedList.actions
+
+export default storedList.reducer
