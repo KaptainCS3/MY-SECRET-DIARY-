9a3d12f8ac7c8831a320b0a8318629a3df9065ad
KaptainCS3 <mbiapplegryn@gmail.com>
2023-07-22 23:37:05 +0100

    deploy for test hotfix needed

--- src/components/SearchDiary.tsx
+++ src/components/SearchDiary.tsx
@@ -1,24 +1,122 @@
+import { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import * as Yup from "yup";
 import ErrorMSG from "./ErrorMSG";
-// interface FormValues {
-//   diarySearch: string;
-// }
+import { collection, query, where, getDocs, orderBy } from "firebase/firestore";
+import { db, auth } from "../utils/firebase";
+import { useAppDispatch } from "../hooks/hook";
+import { diaryListItems } from "../features/DiaryList";
+
+interface FormValues {
+  diarySearch: string;
+  category: string;
+}
+interface User {
+  uid: string;
+}
+
 const SearchDiary = () => {
-  const formik = useFormik({
+  const user = auth.currentUser as User | null;
+  const dispatch = useAppDispatch();
+  const [fetching, setFetching] = useState<boolean>(true);
+  const [categoryOption, setCategoryOption] = useState<object>([]);
+  const formik = useFormik<FormValues>({
     initialValues: {
       diarySearch: "",
+      category: "",
     },
     validationSchema: Yup.object({
       diarySearch: Yup.string().required("Required"),
     }),
     onSubmit: ({ diarySearch }) => {
-      alert(JSON.stringify(diarySearch, null, 2));
-      // formik.resetForm();
+      console.log(diarySearch);
     },
   });
+
+  const getCategory = async () => {
+    const option = collection(db, "category");
+    try {
+      const querySnapshot = await getDocs(option);
+      const optionList = querySnapshot.docs.map((doc) => doc.data());
+      setCategoryOption(optionList[0]["options"]);
+    } catch (error) {
+      console.error("Error getting diary entries: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getCategory();
+  }, []);
+
+  // useEffect(() => {
+  //   const fetchDiaryEntries = async () => {
+  //     const ref = collection(db, "diary");
+  //     const userID = user?.uid;
+  //     if (!userID) {
+  //       throw new Error("User ID is undefined");
+  //     }
+  //     const userEntriesQuery = query(
+  //       ref,
+  //       where("userID", "==", userID),
+  //       where("category", "==", formik.values.category), // filter by category
+  //       orderBy("userID"),
+  //       orderBy("createdDate", "desc")
+  //     );
+
+  //     const publicEntriesQuery = query(
+  //       ref,
+  //       where("isPublic", "==", true),
+  //       where("userID", "!=", userID),
+  //       where("category", "==", formik.values.category), // filter by category
+  //       orderBy("userID"),
+  //       orderBy("createdDate", "desc")
+  //     );
+  //     try {
+  //       const [userEntriesSnapshot, publicEntriesSnapshot] = await Promise.all([
+  //         getDocs(userEntriesQuery),
+  //         getDocs(publicEntriesQuery),
+  //       ]);
+
+  //       const userEntries = userEntriesSnapshot.docs.map((doc) => {
+  //         const data = doc.data();
+  //         const createdDate = data.createdDate.toDate();
+  //         return {
+  //           id: doc.id,
+  //           image: data.image,
+  //           category: data.category,
+  //           description: data.description,
+  //           isPublic: data.isPublic,
+  //           createdDate,
+  //           userID: data.userID,
+  //         };
+  //       });
+  //       console.log("user's entries :", userEntries);
+
+  //       const publicEntries = publicEntriesSnapshot.docs.map((doc) => {
+  //         const data = doc.data();
+  //         const createdDate = data.createdDate.toDate();
+  //         return {
+  //           id: doc.id,
+  //           image: data.image,
+  //           category: data.category,
+  //           description: data.description,
+  //           isPublic: data.isPublic,
+  //           createdDate,
+  //           userID: data.userID,
+  //         };
+  //       });
+  //       console.log("public entries :", publicEntries);
+  //       const diaryEntries = [...userEntries, ...publicEntries];
+  //       dispatch(diaryListItems(diaryEntries));
+  //       setFetching(false);
+  //     } catch (error) {
+  //       console.error("Error getting diary entries: ", error);
+  //     }
+  //   };
+  //   // fetchDiaryEntries();
+  // }, [formik.values.category]);
 
   return (
     <section>
@@ -36,16 +134,24 @@ const SearchDiary = () => {
             </button>
           </div>
           <div className="w-[14%] border-b border-black flex justify-center items-center">
-            <button
-              type="button"
-              className="p-3 pb-[0.5rem] pr-2 cursor-pointer"
+            <select
+              {...formik.getFieldProps("category")}
+              id="category"
+              className="w-full cursor-pointer appearance-none custom py-[0.6rem] px-4 outline-none italic"
             >
-              <img src="/assets/filter.png" alt="" />
-            </button>
+              {Array.isArray(categoryOption) &&
+                categoryOption?.map((el: string) => {
+                  return (
+                    <option value={el} key={el}>
+                      {el}
+                    </option>
+                  );
+                })}
+            </select>
           </div>
         </div>
         <div className="">
-          {formik.errors.diarySearch ? (
+          {formik.errors.diarySearch && formik.touched.diarySearch ? (
             <ErrorMSG error_value={formik.errors.diarySearch} />
           ) : null}
         </div>
